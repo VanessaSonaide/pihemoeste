fe8aa5f79fbb22f5e7783276ab6e07b3e7e8775a
VanessaSonaide <102624971+VanessaSonaide@users.noreply.github.com>
2022-09-02 10:28:43 -0300

    Update welcome.blade.php

--- welcome.blade.php
+++ welcome.blade.php
@@ -7,16 +7,18 @@
     <title>Hemoeste</title>
 </head>
 <body>
+    <img src="{{asset('img-resource/hemoeste.png')}}">
+    <!-- <img src="{{asset('img-resource/avatar.png')}}" alt=""> -->
     <center><h1>Agora você pode acessar o Portal do Hemoeste na sua casa</h1>
     <img src = "https://imagens.ebc.com.br/h8DvueXOfVYK6DcobKSa9rg-nf8=/1170x700/smart/https://agenciabrasil.ebc.com.br/sites/default/files/thumbnails/image/51246851991_c90daf8bc5_o.jpg?itok=2JJK0KjJ">
     <form action= "{{url('/login')}}">
-        <button>Central do doador</button></center>
+        <button>Central do Doador</button></center>
     </form>
 </body>
 <style>
     html{
-        background-color: #710C04;
-        color: white;
+        background-color: #d3d3d3;
+        color: #710C04;
         margin: 10px;
     }
     img{
@@ -24,8 +26,9 @@
         height: 400px;
     }
     button{
+        background-color: #710C04;
         font-weight: bold;
-        color: #710C04;
+        color: white;
         height: 30px;
     }
 </style>
