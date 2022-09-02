08e2e57f8285b5ef57e0091062ceaaafb12bbd32
VanessaSonaide <102624971+VanessaSonaide@users.noreply.github.com>
2022-09-02 08:24:44 -0300

    Create welcome.blade.php

---
+++ welcome.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hemoeste</title>
+</head>
+<body>
+    <center><h1>Agora você pode acessar o Portal do Hemoeste na sua casa</h1>
+    <img src = "https://imagens.ebc.com.br/h8DvueXOfVYK6DcobKSa9rg-nf8=/1170x700/smart/https://agenciabrasil.ebc.com.br/sites/default/files/thumbnails/image/51246851991_c90daf8bc5_o.jpg?itok=2JJK0KjJ">
+    <form action= "{{url('/login')}}">
+        <button>Central do doador</button></center>
+    </form>
+</body>
+<style>
+    html{
+        background-color: #710C04;
+        color: white;
+        margin: 10px;
+    }
+    img{
+        margin: 15px;
+        height: 400px;
+    }
+    button{
+        font-weight: bold;
+        color: #710C04;
+        height: 30px;
+    }
+</style>
+</html>
